40f3283039f87f968bc2fdb148e5c1d991fb0624
Arijit Aich <a2.arijitaich@gmail.com>
2025-05-06 01:52:35 +0530

    Updated Register Device API Endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate  # Import Flask-Migrate
 from .config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()  # Initialize Flask-Migrate
 
 def create_app():
     # Ensure templates folder is found correctly
@@ -15,6 +17,7 @@ def create_app():
 
     # Initialize database
     db.init_app(iot_app)
+    migrate.init_app(iot_app, db)  # Bind Flask-Migrate to the app and db
 
     # Register all blueprints
     from .api_routes import api_bp
@@ -28,7 +31,6 @@ def create_app():
     # Automatically create tables if they don't exist
     with iot_app.app_context():
         from app import models  # This ensures all models are loaded
-                                # This ensures all models are loaded
         db.create_all()
     
     return iot_app
